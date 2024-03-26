524caff0bd1cea6755837b1209bd24e81e9b4696
Cleidson <cleidson.mac@outlook.com>
2024-03-26 11:36:07 -0400

    Add Basic Swagger Files and Stuf for Configurations

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const userRoutes = require("./src/core/event-login/registration-scope/user/user.
 const deliveryAddressRoutes = require("./src/core/event-login/registration-scope/delivery-address/delivery-address.routes");
 const userLoginRoute = require("./src/core/event-login/registration-scope/user-auth/user-auth.routes");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./src/swagger-output.json"); //## HERE! AUTO GENERATION!
+
 const port = 3000;
 
 app.use(express.json());
@@ -17,11 +20,15 @@ app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: false }));
 
+//## API ROUTES
 app.use("/v1/api/pizza", pizzaRoutes);
 app.use("/v1/api/pizzaSales", pizzaSalesOrderRoutes);
 app.use("/v1/api/user", userRoutes);
 app.use("/v1/api/deliveryAddress", deliveryAddressRoutes);
 app.use("/v1/api/auth", userLoginRoute);
+
+//## HERE Start the Swagger Document API
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   mongoConnDb().then(() => {
